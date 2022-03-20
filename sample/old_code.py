f323ed85fd9245ad27e94ad041db2aa4d8465366
Prajin Khadka <34759925+prajinkhadka@users.noreply.github.com>
2022-03-20 13:20:07 +0545

    added sample

---
+++ sample/old_code.py
@@ -0,0 +1,12 @@
+def lineearSearch():
+    print("this is a sorting fucntion")
+    
+def Quicksort():
+    print("new function")
+    
+def binary_search(a):
+    return a 
+
+s1 = lineearSearch()
+s2 = Quicksort()
+s3 = binary_search(1)
---
+++ sample/old_code_new_.py
@@ -0,0 +1,11 @@
+def lineearSearch_my_new_func():
+    print('this is a sorting fucntion')
+
+def Quicksort_my_new_func():
+    print('new function')
+
+def binary_search_my_new_func(a):
+    return a
+s1_my_new_var = lineearSearch_my_new_func()
+s2_my_new_var = Quicksort_my_new_func()
+s3_my_new_var = binary_search_my_new_func(1)
